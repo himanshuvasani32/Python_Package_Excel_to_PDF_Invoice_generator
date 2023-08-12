df324cfb79455fbb0d5b77f0931f6568d4fd1b1a
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-08-12 12:58:51 +0530

    Build final package

--- invoicing/invoice.py
+++ invoicing/invoice.py
@@ -5,7 +5,20 @@ from fpdf import FPDF
 from pathlib import Path
 
 
-def generator(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased, price_per_unit, total_price):
+def generator(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased,
+              price_per_unit, total_price):
+    """
+    This function converts invoice excel files into PDF invoices.
+    :param invoices_path:
+    :param pdfs_path:
+    :param image_path:
+    :param product_id:
+    :param product_name:
+    :param amount_purchased:
+    :param price_per_unit:
+    :param total_price:
+    :return:
+    """
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
 
     for filepath in filepaths:
